37a817f86103c628d7183750e2da24725979d1b9
durgaprasad253 <durgaprasadd253@gmail.com>
2021-12-13 00:09:40 +0530

    added imagefield in database models

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title=models.CharField(max_length=50)
     excert=models.CharField(max_length=200)
-    image_name=models.CharField(max_length=50)
+    image_name=models.ImageField(upload_to="posts")
     date=models.DateField(auto_now=True)
     slug=models.SlugField(unique=True,db_index=True)
     author=models.ForeignKey(Author,on_delete=models.SET_NULL,null=True,related_name="posts")
